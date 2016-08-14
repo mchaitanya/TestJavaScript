830f7337f76f3aa6e2faa652794606b6eff01ab4
mchaitanya <chaitanyaaa@gmail.com>
2016-08-14 01:28:56 -0500

    1: Switch to recursion-based method implementations Updated the listed methods to use recursion.

--- chap04_exer04.js
+++ chap04_exer04.js
@@ -7,19 +7,39 @@
  */
 
 // ARRAY TO LIST
-function arrayToList(array) {
-	var list = {};
-	for (var i = 0, node = list; ; i++, node = node.rest) {
-		node.value = array[i];
-		if (i == array.length-1) { // on last element
-			node.rest = null;
-			break;
-		} else {
-			node.rest = {};
-		}
-	}
+//function arrayToList(array) {
+//	var list = {};
+//	for (var i = 0, node = list; ; i++, node = node.rest) {
+//		node.value = array[i];
+//		if (i == array.length-1) { // on last element
+//			node.rest = null;
+//			break;
+//		} else {
+//			node.rest = {};
+//		}
+//	}
+//
+//	return list;
+//}
 
-	return list;
+//recursive
+function arrayToList(array) {
+var list = {};
+// you can recursively process sub-arrays that get smaller with every pass
+function convert(array, list) {
+  if (array.length > 0) {
+    list.value = array[0];
+    if (array.length > 1) {
+      list.rest = {};
+      convert(array.slice(1), list.rest); // recursion
+    } else {
+      list.rest = null;
+    }
+  }
+}
+convert(array, list);
+
+return list;
 }
 
 console.log(arrayToList([10, 20]));
@@ -27,12 +47,27 @@ console.log(arrayToList([10, 20]));
 
 
 // LIST TO ARRAY
+//function listToArray(list) {
+//  var array = [];
+//  for (var node = list; node != null; node = node.rest) {
+//    array.push(node.value);
+//  }
+//
+//  return array;
+//}
+
+//recursive
 function listToArray(list) {
   var array = [];
-  for (var node = list; node != null; node = node.rest) {
-    array.push(node.value);
+  // you can recursively process the tail of sub-lists that get smaller with every pass
+  function convert(list) {
+    if (list != null) {
+      array.push(list.value);
+      convert(list.rest); // recursion
+    }
   }
-
+  convert(list);
+  
   return array;
 }
 
@@ -50,16 +85,26 @@ console.log(prepend(10, prepend(20, null)));
 
 
 // GET THE NTH ELEMENT OF THE LIST
-function nth(list, pos) {
-  var elem = undefined;
-  for (var i = 0, node = list; node != null ; i++, node = node.rest) {
-    if (i == pos) {
-      elem = node.value;
-      break;
-    }
-  }
+//function nth(list, pos) {
+//  var elem = undefined;
+//  for (var i = 0, node = list; node != null ; i++, node = node.rest) {
+//    if (i == pos) {
+//      elem = node.value;
+//      break;
+//    }
+//  }
+//
+//  return elem;
+//}
 
-  return elem;
+//recursive
+function nth(list, pos) {
+if (list == null) 
+  return undefined;
+else if (pos == 0)
+  return list.value;
+else 
+  return nth(list.rest, pos-1);
 }
 
 console.log(nth(arrayToList([10, 20, 30]), 1));
